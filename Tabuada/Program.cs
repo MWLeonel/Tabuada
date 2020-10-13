9a8c62505e11c90b542f67df6e30de8d66467ab5
MWLeonel <wmleonel6@gmail.com>
2020-10-13 18:37:12 -0300

    Tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um Número: ");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine(numero + " * " + x + " = " + numero * x);
+            }
+        }
+    }
+}
